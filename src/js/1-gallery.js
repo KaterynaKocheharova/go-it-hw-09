af236084e6fd063e1882853c74bf01e6b5f9210c
KaterynaKocheharova <kot41901@gmail.com>
2024-02-14 13:27:20 +0200

    included libraryn correctly

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,4 @@
 // ============================================ INCLUDING SIMPLE LIGHTBOX ======================================
-// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-// // import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,2 @@
-// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-// // import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
