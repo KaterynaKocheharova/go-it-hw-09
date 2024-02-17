9a97568cab3c3a274b74c0f7013df6c327eecd7b
KaterynaKocheharova <kot41901@gmail.com>
2024-02-17 17:24:53 +0200

    form homeword task 1 partly done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,35 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// MAIN VARIABLES
+
+const form = document.querySelector('.feedback-form');
+const email = form.email;
+const message = form.message;
+
+// SETTING LOCAL STORAGE
+
+const storageKey = 'feedback-form-state';
+
+const formDataObject = JSON.parse(localStorage.getItem(storageKey)) || {
+  email: '',
+  message: '',
+};
+
+// SETTING VALUE TO FORM FIELDS DURING PAGE LOAD
+email.value = formDataObject.email;
+message.value = formDataObject.message;
+
+// ADDING DATA TO LOCAL STORAGE
+
+form.addEventListener('input', event => {
+  const currentData = JSON.parse(localStorage.getItem(storageKey));
+
+  if (event.target === form.email) {
+    currentData.email = event.target.value;
+  }
+
+  if (event.target === form.message) {
+    currentData.message = event.target.value;
+  }
+
+  const updatedData = JSON.stringify(currentData);
+  localStorage.setItem(storageKey, updatedData);
+});
